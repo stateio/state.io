67ff831e112f23cb411076e9127d6d8736215c6c
Phill MV <phillmv@okayfail.com>
2012-08-02 13:00:17 -0400

    Added deploy script.

---
+++ config/deploy.rb
@@ -0,0 +1,19 @@
+set :application, "state.io"
+set :repository,  "git@github.com:phillmv/state.io.git"
+set :use_sudo, false
+set :scm, :git
+set :deploy_to, "/var/www/#{application}"
+
+role :web, "okayfail.com"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
