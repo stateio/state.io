ff76c7c174256768770ecef3e4d8e3d81969238f
Max Veytsman <maxim@ontoillogical.com>
2013-02-28 02:59:33 -0500

    Deploy cap script now works with our new infastructure

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,11 @@ set :application, "state.io"
 set :repository,  "git@github.com:stateio/state.io.git"
 set :use_sudo, false
 set :scm, :git
-set :deploy_to, "/var/www/#{application}"
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
 
-role :web, "phillmv@okayfail.com"
+set :user, "deploy"
+role :web 'okayfail.com'
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
