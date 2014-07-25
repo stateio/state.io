530973323a84f402cb7f02730e5c8e8d3b4debab
Phill MV <phillmv@okayfail.com>
2014-07-25 14:27:21 -0400

    Changed bio pics, new cap conf, finalized bs3

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,21 +1,25 @@
+require "bundler/capistrano"
+require "rvm/capistrano"
+set :rvm_ruby_string, 'ruby-1.9.3'
+set :rvm_type, :user
 set :application, "state.io"
 set :repository,  "git@github.com:stateio/state.io.git"
 set :use_sudo, false
 set :scm, :git
 ssh_options[:forward_agent] = true
 default_run_options[:pty] = true
+set :branch, ENV['BRANCH'] || 'master'
 
 set :user, "deploy"
-role :web, 'okayfail.com'
+role :web, "okayfail.com"
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  task :bundle do
+    run "cd #{current_path} && bundle install"
+  end
+  task :run_md do
+    run "cd #{current_path} && bundle exec middleman build"
+  end
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after "deploy:restart", "deploy:run_md"
